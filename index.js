a6cd5cf543a8c1c6fab0e074fbfe78d221ad81df
Jose M. Polanco (Darkcode01) <j0s3s3gur4@gmail.com>
2019-04-25 15:05:39 -0400

    Se agrego la parte de modals para el chat privado de los usuario.
    falta : chat privado

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const io = require('socket.io')(server);
 const axios = require('axios');
 const crypto = require('crypto');
 
+var users = {};
+
 app.use(express.static('semantic'));
 
 app.get('/', async (req, res) => {
@@ -23,11 +25,47 @@ app.get('/gravatar', async (req, res) => {
   });
 });
 
+app.get('/private', async (req, res) => {
+  const username = req.query.u;
+
+
+  res
+    .json({
+      error: 'nNot Found!'
+    })
+});
 
 io.on('connection', (socket) => {
+
+  socket.on('thing', (id, msg) => {
+    io.to(socket.id).emit('me', id);
+  });
+
+  socket.on('join user', data => {
+    users[data.username] = {
+      id: socket.id,
+      gravatar: data.gravatar
+    };
+
+    io.emit('joined users', users);
+  });
+
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
-    console.log(msg);
+  });
+
+  socket.on('disconnect', reason => {
+    let username = null;
+
+    for (let user in users) {
+        if (users[user].id == socket.id) {
+          username = user;
+          break;
+        }
+    }
+
+    delete users[username];
+    io.emit('joined users', users);
   });
 });
 
--- semantic/dist/utils.js
+++ semantic/dist/utils.js
@@ -1,17 +1,33 @@
-const button_send = document.getElementById('send');
 const button = document.getElementById('button');
 const dashboard = document.getElementById('dashboard');
-let chat_box = document.getElementById('chat-box');
 var data = {};
 
 const client = io();
 
 client.on('connect', socket => {
   data.id = client.id;
+
+  client.emit('thing', data.id, 'asdasd');
+});
+
+client.on('joined users', users => {
+  let feed = document.getElementById('feed');
+  feed.className = 'ui feed';
+  let modals = document.getElementById('modals');
+  // remove data
+  _removeChild(feed);
+
+  for (let user in users) {
+    if (user != data.username) {
+      feed.append(createItemuser(user, users[user]));
+      modals.append(createModal(user, users[user]));
+    }
+  }
 });
 
 client.on('chat message', msg => {
-  let comment = document.createElement('div');
+  let chat_box = document.getElementById('chat-box');
+  let item = document.createElement('div');
   let gravatar = document.createElement('a');
   let image = document.createElement('img');
   let content = document.createElement('div');
@@ -19,11 +35,11 @@ client.on('chat message', msg => {
   let text = document.createElement('p');
 
   // add className
-  comment.className += ' comment';
-  gravatar.className = 'avatar';
+  item.className = ' item';
+  gravatar.className = 'ui tiny image';
   content.className = 'content';
-  author.className = 'author';
-  text.className = 'text';
+  author.className = 'header';
+  text.className = 'description';
 
   // set src
   image.setAttribute('src', msg.gravatar);
@@ -38,15 +54,21 @@ client.on('chat message', msg => {
   content.appendChild(author);
   content.appendChild(text);
 
-  comment.appendChild(gravatar);
-  comment.appendChild(content);
+  item.appendChild(gravatar);
+  item.appendChild(content);
 
-  chat_box.appendChild(comment);
+  chat_box.appendChild(item);
 });
+
+client.on('me', msg => {
+  console.log(msg);
+})
+
 
 button.addEventListener('click', async (event) => {
   const username = document.getElementById('username');
 
+  let chat = document.getElementById('chat-normal')
   let card = document.getElementById('card');
   let loading = document.getElementById('loading');
   let gravatar = document.getElementById('gravatar');
@@ -61,18 +83,25 @@ button.addEventListener('click', async (event) => {
   loading.style.display = "";
 
   setTimeout(() => {
+    chat.append(createChat());
+    var button_send = document.getElementById('send');
+    button_send.onclick = sendMessage;
     loading.style.display = "none";
     dashboard.style.display = ""
 
     gravatar.setAttribute('src', response.data.gravatar);
     username_info.innerHTML = username.value;
-    id_.innerHTML = data.id;
     data.username = username.value;
     data.gravatar = response.data.gravatar;
+
+    client.emit('join user', {
+      username: data.username,
+      gravatar: data.gravatar
+    });
   }, 500);
 });
 
-button_send.addEventListener('click', async (event) => {
+function sendMessage(event) {
   const message = document.getElementById('message').value;
 
   client.emit('chat message', {
@@ -81,4 +110,128 @@ button_send.addEventListener('click', async (event) => {
     id: data.id,
     username: data.username
   });
-});
+};
+
+
+function _removeChild(node) {
+  while (node.hasChildNodes()) {
+    node.removeChild(node.lastChild);
+  }
+}
+
+function createChat(private = false) {
+  let chat = document.createElement('div');
+  let header = document.createElement('div');
+  let divider = document.createElement('div');
+  let messages = document.createElement('div');
+  let _form = document.createElement('div');
+  let form = document.createElement('div');
+  let field = document.createElement('div');
+  let textarea = document.createElement('textarea');
+  let button = document.createElement('button');
+
+  // styles and class
+  divider.className = 'ui divider';
+  chat.className = 'ui segment';
+  chat.style.width = '805px';
+  header.className = 'ui centered huge header';
+  messages.className = 'ui items';
+  messages.setAttribute('id', private ? 'chat-private' : 'chat-box');
+  messages.style.height = '400px';
+  messages.style['overflow-y'] = 'auto';
+  _form.className = private ? '_form_private' : '_form';
+  form.className = 'ui form';
+  field.className = 'field';
+  textarea.setAttribute('id', private ? 'message_private' : 'message');
+  button.className = 'ui button';
+  button.setAttribute('id', private ? 'send_private' : 'send');
+
+  // add content
+  button.append('Enviar Mensaje');
+  header.append('Chat');
+  field.appendChild(textarea);
+  form.appendChild(field);
+  form.appendChild(button);
+  _form.appendChild(form);
+  chat.appendChild(header);
+  chat.appendChild(divider);
+  chat.appendChild(messages);
+  chat.appendChild(divider);
+  chat.appendChild(_form);
+
+  return chat;
+}
+
+function createModal(user, data) {
+  let modal = document.createElement('div');
+  let icon = document.createElement('i');
+  let header = document.createElement('div');
+  let content = document.createElement('div');
+  let description = document.createElement('div');
+  let actions = document.createElement('div');
+  let close = document.createElement('div');
+  let chat = createChat(true);
+
+  modal.className = `ui modal ${user}`;
+  icon.className = 'close icon';
+  header.className = 'header';
+  header.append(`Chat ${user}`);
+  description.className = 'description';
+  content.className = 'content';
+  content.append();
+  actions.className = 'actions';
+  close.className = 'ui black deny button';
+  send.className = 'ui right labeled icon button';
+
+  close.append('Cerrar');
+  description.appendChild(chat);
+  content.appendChild(description);
+  actions.appendChild(close);
+  modal.appendChild(icon);
+  modal.appendChild(header);
+  modal.appendChild(content);
+  modal.appendChild(actions);
+
+  return modal;
+}
+
+function createItemuser(user, data) {
+  let _event = document.createElement('div');
+  let image = document.createElement('img');
+  let label = document.createElement('div');
+  let content = document.createElement('div');
+  let summary = document.createElement('div');
+  let _user = document.createElement('a');
+  let date = document.createElement('div');
+  let icon = document.createElement('i');
+  let divider = document.createElement('div');
+
+  // className
+  _event.className = 'event';
+  label.className = 'label';
+  content.className = 'content';
+  summary.className = 'summary';
+  _user.className = `user header`;
+  date.className = 'date';
+  icon.className = 'icon green circle';
+  divider.className = 'ui divider';
+
+  image.setAttribute('src', data.gravatar);
+  _user.setAttribute('id', user);
+  _user.append(user);
+  _user.addEventListener('click', (event) => {
+    $(`.${event.target.id}`)
+      .modal('show');
+  });
+  date.append(icon);
+
+  // makup
+  label.appendChild(image);
+  summary.appendChild(_user);
+  summary.appendChild(date);
+  content.appendChild(summary);
+  _event.appendChild(label);
+  _event.appendChild(content);
+
+  return _event;
+}
